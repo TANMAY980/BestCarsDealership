52b7a9f8b0faadc41b3003b03cb3a342a492f351
Tanmay980 <karmakartanmay980@email.com>
2023-08-06 08:04:08 -0400

    Created carmake and carModel in the model

--- djangoapp/admin.py
+++ djangoapp/admin.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
 # from .models import related models
-
+from .models import CarMake, CarModel
 
 # Register your models here.
 
 # CarModelInline class
-
+class CarModelInline(admin.TabularInline):
+    model = CarModel
 # CarModelAdmin class
-
+@admin.register(CarModel)
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'car_make', 'type', 'year', 'dealer_id']
 # CarMakeAdmin class with CarModelInline
+@admin.register(CarMake)
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    inlines = [CarModelInline]
 
 # Register models here
--- djangoapp/models.py
+++ djangoapp/models.py
@@ -25,3 +25,28 @@ from django.utils.timezone import now
 
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    # Add any other fields you want for CarMake
+
+    def __str__(self):
+        return self.name
+
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    dealer_id = models.IntegerField()
+    name = models.CharField(max_length=100)
+    TYPE_CHOICES = [
+        ('Sedan', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'WAGON'),
+        # Add more choices as needed
+    ]
+    type = models.CharField(max_length=10, choices=TYPE_CHOICES)
+    year = models.DateField()
+    # Add any other fields you want for CarModel
+
+    def __str__(self):
+        return f"{self.car_make.name} - {self.name}"
