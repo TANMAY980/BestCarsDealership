d76a280c4d1c7915945c1f60218ab5f6cde9afef
Tanmay980 <karmakartanmay980@email.com>
2023-08-07 13:18:50 -0400

    fetching the dealership and review

--- djangoapp/admin.py
+++ djangoapp/admin.py
@@ -1,20 +1,22 @@
 from django.contrib import admin
-# from .models import related models
 from .models import CarMake, CarModel
-
-# Register your models here.
-
 # CarModelInline class
-class CarModelInline(admin.TabularInline):
+class CarModelInline(admin.StackedInline):
     model = CarModel
-# CarModelAdmin class
-@admin.register(CarModel)
-class CarModelAdmin(admin.ModelAdmin):
-    list_display = ['name', 'car_make', 'type', 'year', 'dealer_id']
+    extra = 2
+
 # CarMakeAdmin class with CarModelInline
-@admin.register(CarMake)
 class CarMakeAdmin(admin.ModelAdmin):
     list_display = ['name', 'description']
+    search_fields = ['name']
     inlines = [CarModelInline]
 
+# CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ['car_make', 'name', 'dealer_id', 'model_type', 'year']
+    list_filter = ['model_type', 'car_make', 'dealer_id', 'year',]
+    search_fields = ['car_make', 'name']
+
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- djangoapp/urls.py
+++ djangoapp/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
-    path('', views.index_view, name='index'),
+    #path('', views.index_view, name='index'),
     # path for about view
     path('about/', views.about_us_view, name='about-us'),
     # path for contact us view
@@ -21,10 +21,10 @@ urlpatterns = [
     # path for logout
     path('logout/', views.logout_view, name='logout'),
 
-#    path(route='', view=views.get_dealerships, name='index'),
+    path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
-
-    # path for add a review view
-
+    path(route='dealer/<int:dealer_id>/', view=views.get_dealer_details, name='dealer_details'),
+    # path for adding a new review view
+    path(route='dealer/<int:dealer_id>/add-review/', view=views.add_review, name="add_review")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
